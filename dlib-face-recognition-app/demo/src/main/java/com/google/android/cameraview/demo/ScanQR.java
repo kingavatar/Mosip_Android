178f3b72fa52a0fc4b05364cbe3ed0a2876ae401
kingavatar <saikiranreddy06@gmail.com>
2020-06-06 12:03:19 +0530

    Changed Source Files Compatibility

--- dlib-face-recognition-app/demo/src/main/java/com/google/android/cameraview/demo/ScanQR.java
+++ dlib-face-recognition-app/demo/src/main/java/com/google/android/cameraview/demo/ScanQR.java
@@ -15,14 +15,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-
+import com.tzutalin.dlib.Constants;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ScanQR extends AppCompatActivity implements View.OnClickListener {
 
     TextView name, email, dob, address;
-
+    int BITMAP_QUALITY = 100;
+    int MAX_IMAGE_SIZE = 500;
+    String TAG = "AddPerson";
+    private Bitmap bitmap;
+    private File destination = null;
+    private String imgPath = null;
     Button selectImage;
     //qr code scanner object
     private IntentIntegrator qrScan;
@@ -36,6 +47,7 @@ public class ScanQR extends AppCompatActivity implements View.OnClickListener {
         //intializing scan object
         qrScan = new IntentIntegrator(this);
         findViewsById();
+        qrScan.initiateScan();
     }
 
 
@@ -73,10 +85,27 @@ public class ScanQR extends AppCompatActivity implements View.OnClickListener {
                     name.setText(obj.getString("name"));
                     email.setText(obj.getString("email"));
                     dob.setText(obj.getString("dob"));
+                    address.setText(obj.getString("address"));
                     String base64Image = obj.getString("image");
                     byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
                     Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                    Bitmap scaleddecodedByte = AddPerson.scaleDown(decodedByte,MAX_IMAGE_SIZE,true);
                     imageView.setImageBitmap(decodedByte);
+                    String targetPath = Constants.getDLibImageDirectoryPath() + "/" + name.getText().toString() + ".jpg";
+                    destination = new File(targetPath);
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                    scaleddecodedByte.compress(Bitmap.CompressFormat.JPEG, BITMAP_QUALITY, bytes);
+                    FileOutputStream fo;
+                    try {
+                        destination.createNewFile();
+                        fo = new FileOutputStream(destination);
+                        fo.write(bytes.toByteArray());
+                        fo.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -95,8 +124,10 @@ public class ScanQR extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         //initiating the qr code scan
-        qrScan.initiateScan();
+        Intent i = new Intent(ScanQR.this,MainActivity.class);
+        startActivity(i);
     }
 
-
 }
+
+
